e753e01285eea6f91eaa6b1f82866290c4bba1ac
Karan_Paul <karan.paul@fnp.com>
2024-12-19 16:31:59 +0530

    random color

--- js/custom.js
+++ js/custom.js
@@ -50,3 +50,38 @@ if (screen.width <= 768) {
   const mainHeader = document.querySelector(".mainHeader");
   mainHeader && createHm(mainHeader);
 }
+
+/**smooth scrolling */
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    const hmMenu = document.querySelector(".hm");
+    const nav = document.querySelector("nav");
+    hmMenu.classList.toggle("open");
+    nav.classList.toggle("open");
+    const targetElement = document.querySelector(this.getAttribute("href"));
+    const offsetPosition =
+      targetElement.getBoundingClientRect().top + window.pageYOffset - 132;
+    nav.classList.remove("open");
+    window.scrollTo({
+      top: offsetPosition,
+      behavior: "smooth",
+    });
+  });
+});
+
+/**random colors */
+
+const colors = ["pink", "black", "brown", "lightPink"];
+const galleryList = document.querySelectorAll(
+  ".gallery ul li .imgSec .imgText"
+);
+// Iterate through each element in galleryList
+galleryList.forEach((element) => {
+  // Pick a random color from the colors array
+  const randomColor = colors[Math.floor(Math.random() * colors.length)];
+
+  // Add the random color as a class to the element
+  element.classList.add(randomColor);
+});
